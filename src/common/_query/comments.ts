ccac08b80b41766441882ad33d1adb120a790be7
JY Park <loput12ouj35@gmail.com>
2022-10-25 21:36:05 +0900

    chore: eslint에 타입스크립트 세팅

--- src/common/_query/comments.ts
+++ src/common/_query/comments.ts
@@ -3,6 +3,5 @@ import useSWR, { SWRResponse } from 'swr'
 import { getComments } from '_api/comment'
 import { Comment } from 'common/types/comment'
 
-// TODO: 리턴 타입 명시
 export const useCommentListQuery = (queryString: string): SWRResponse<Comment[]> =>
   useSWR(['comments', queryString], () => getComments(queryString))
