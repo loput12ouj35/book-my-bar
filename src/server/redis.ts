36af0295e4ffc4d322a9bf1bb2612f205aea11da
JY Park <loput12ouj35@gmail.com>
2022-10-28 00:40:07 +0900

    fix: 노드 버전 다운그레이드 및 폴리필 추가

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -1,6 +1,15 @@
 import { Ratelimit } from '@upstash/ratelimit'
 import { Redis } from '@upstash/redis'
 
+// polyfill. redis가 atob 쓰는데, node 17이하 버전에는 atob가 없음.
+if (typeof btoa === 'undefined') {
+  global.btoa = (b) => Buffer.from(b).toString('base64')
+}
+
+if (typeof atob === 'undefined') {
+  global.atob = (a) => Buffer.from(a, 'base64').toString('binary')
+}
+
 export const redis = Redis.fromEnv()
 
 // Create a new ratelimiter, that allows 5 requests per 5 seconds
